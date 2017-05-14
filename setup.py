394ed21ad045e001ce2658cbd1b757b185a072d2
koji-kojiro <kojiro0531@gmail.com>
2017-05-14 19:52:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from __future__ import print_function
+import atexit
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class install_with_compile_hy(install):
+    def __init__(self, *args, **kwargs):
+        install.__init__(self, *args, **kwargs)
+        atexit.register(self._compile_hy)
+
+    def _compile_hy(self):
+        import hy
+        import hy_compiler.util
+        import hy_compiler.core
+
+        print("hy version: {}".format(hy.__version__))
+        print("compiled: {}".format(hy_compiler.util.__file__))
+        print("compiled: {}".format(hy_compiler.core.__file__))
+
 
 config = {
     'name': 'hy-compiler',
@@ -19,7 +38,16 @@ config = {
         "License :: OSI Approved :: MIT License",
         "Development Status :: 1 - Planning",
     ],
-    'packages': find_packages(),
+    'packages': ['hy_compiler', 'hy_compiler.core'],
+    'package_data': {
+        'hy_compiler': ['*.hy'],
+        'hy_compiler.core': ['*.hy'],
+    },
+    'entry_points':
+    '[console_scripts]\nhy-compiler=hy_compiler.util:hy_compiler_main',
+    'cmdclass': {
+        'install': install_with_compile_hy
+    },
 }
 
 if __name__ == '__main__':
