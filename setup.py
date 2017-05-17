8942ff8a7c05fc867f5d375e1ce61abe1df39b99
koji-kojiro <kojiro0531@gmail.com>
2017-05-17 13:10:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import atexit
 from setuptools import setup
-from setuptools.command.install import install
+from setuptools.command.install import _install
 
 from sys import version_info
 ver = version_info[0]
 
 
-class install_with_compile_hy(install):
+class install(_install):
     def __init__(self, *args, **kwargs):
-        install.__init__(self, *args, **kwargs)
+        import pip
+        import atexit
+
+        pip.main("install git+https://github.com/hylang/hy".split())
+
+        _install.__init__(self, *args, **kwargs)
         atexit.register(self._compile_hy)
 
     def _compile_hy(self):
+        import os
         import hy
-        import hy_compiler.util
-        import hy_compiler.core.build
-        import hy_compiler.core.translate
+        import hy_compiler
 
         print("hy version: {}".format(hy.__version__))
-        print("compiled: {}".format(hy_compiler.util.__file__))
-        print("compiled: {}".format(hy_compiler.core.build.__file__))
-        print("compiled: {}".format(hy_compiler.core.translate.__file__))
+
+        for dirname, _, filenames in os.walk(
+                os.path.dirname(hy_compiler.__file__)):
+            for filename in filenames:
+                if filename.endswith('.hy'):
+                    print('compiling: {}'.format(filename))
+                    hy.importer.write_hy_as_pyc(
+                        os.path.join(dirname, filename))
 
 
 config = {
@@ -35,7 +43,7 @@ config = {
     'long_description': open('README.rst', 'r').read(),
     'license': 'MIT',
     'version': '0.0.1',
-    'install_requires': [],
+    'install_requires': ['Cython>=0.25.2', 'hy>=0.12.1'],
     'classifiers': [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
@@ -51,11 +59,11 @@ config = {
     'entry_points': {
         'console_scripts': [
             'hy-compiler=hy_compiler.util:hy_compiler_main',
-            'hy-compiler%s=hy_compiler.util:hy_compiler_main' % ver,
+            'hy-compiler%d=hy_compiler.util:hy_compiler_main' % ver,
         ]
     },
     'cmdclass': {
-        'install': install_with_compile_hy
+        'install': install
     },
 }
 
